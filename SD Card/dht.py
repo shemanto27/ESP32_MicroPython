798a428dd6bbbe516579a6715bc1f3fdbdab6270
Bidut Sharkar Shemanto <84231263+shemanto27@users.noreply.github.com>
2024-02-05 22:35:13 +0600

    Update dht.py

--- SD Card/dht.py
+++ SD Card/dht.py
@@ -15,20 +15,27 @@ os.mount(vfs, "/fc")
 print("Filesystem check")
 print(os.listdir("/fc"))
 
+fn = "/fc/data-logger.csv"
+with open(fn, "a") as f:
+            f.write("Time,")
+            f.write("Temp,")
+            f.write("Humidity\n")
+            f.close()
+
 while True:
     try:
         sensor.measure()
         t = sensor.temperature()
         h = sensor.humidity()
         time_tuple = localtime()
-        fn = "/fc/data-logger.csv"
+        
         
         # Open the file for appending and write a line of text to it
         with open(fn, "a") as f:
-            f.write("{}-{:02d}-{:02d} {:02d}:{:02d}:{:02d},".format(
+            f.write("{}-{:02d}-{:02d} {:02d}:{:02d}:{:02d},".format(       
                 time_tuple[0], time_tuple[1], time_tuple[2],
-                time_tuple[3], time_tuple[4], time_tuple[5]))
-            f.write("{:.2f},".format(t))
+                time_tuple[3], time_tuple[4], time_tuple[5])) # {}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}: This is formatting for the date and time
+            f.write("{:.2f},".format(t))  # .2f: This means format as a floating-point number (f) with 2 digits after the decimal point (.2)
             f.write("{:.2f}\n".format(h))
 
         sleep(2)
